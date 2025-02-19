387fade59c7e6451002985038f9870f3b2ba54b9
ayalanur_ <156872566+oceandrive8@users.noreply.github.com>
2025-02-19 11:27:17 +0500

    Delete Lab 5/MyShop2 directory

--- Lab 5/MyShop2/src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { ProductListComponent } from './product-list/product-list.component';
-
-export const routes: Routes = [
-  { path: '', redirectTo: 'products', pathMatch: 'full' }, 
-  { path: 'products', component: ProductListComponent }    
-];
-
